6cd02cb2e41708b2a6a712a46fabbd098dee3659
marchmichels <56034198+marchmichels@users.noreply.github.com>
2022-09-20 16:28:16 -0400

    Created repository
    Created a repository based off files from the blog case study.

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+//initial setting to make sure all errors will be displayed.
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require __DIR__."/../core/bootstrap.php";
+
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Config File
+ */
+$db = [
+    'host' => 'localhost',
+    'database' => 'blog',
+    'username' => 'I425User',
+    'password' => 'WebServices',
+    'port' => 3306
+];
+
